c8f64fcce426f6b6c0e4d32d5737f3419ebce6ea
ilya-beginner <ilya.beginner@gmail.com>
2022-11-17 02:49:21 +0300

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-set +e
-set +x
-set +v
-
-docker-compose -p expenses -f expenses-backend.yaml -f expenses-ui.yaml -f expenses-database.yaml down
-docker-compose -p expenses -f expenses-backend.yaml -f expenses-ui.yaml -f expenses-database.yaml up -d
